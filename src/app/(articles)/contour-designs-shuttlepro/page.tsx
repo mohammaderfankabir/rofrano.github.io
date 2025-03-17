10069c16283ad5a5764c83f8ea616d09cc370ff5
Mohammaderfan Kabir <monie.kabir@gmail.com>
2025-03-18 01:59:47 +0330

    Rename contour-designs-shuttlepro

---
+++ src/app/(articles)/contour-designs-shuttlepro/page.tsx
@@ -0,0 +1,7 @@
+export default function ContourDesignsShuttlepro() {
+  return (
+    <div>
+      <h1>ContourDesignsShuttlepro</h1>
+    </div>
+  );
+}
--- src/app/(articles)/shuttlepro-contour-designs/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ShuttleproContourDesigns() {
-  return (
-    <div>
-      <h1>ShuttleproContourDesigns</h1>
-    </div>
-  );
-}
